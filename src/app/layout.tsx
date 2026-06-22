dbcee86aa7d126938f9b10d9f92c3749d7c6cb17
Jitendra Maharana <maharanajitendra1996@gmail.com>
2026-06-23 02:58:08 +0530

    added multitag-banner-ad

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -148,6 +148,21 @@ l.parentNode.insertBefore(s, l);
           }}
           strategy="afterInteractive"
         />
+        <Script
+          dangerouslySetInnerHTML={{
+            __html: `(function(fgvbk){
+var d = document,
+    s = d.createElement('script'),
+    l = d.scripts[d.scripts.length - 1];
+s.settings = fgvbk || {};
+s.src = "\/\/massivesalad.com\/b-X_V.sydTGbla0\/Y\/WmcN\/Kekme9\/u\/ZqU\/lckjPFTlcexLNAjTEf0EOfT_cftZNAzzEW2\/MCTaU\/wVM\/Qu";
+s.async = true;
+s.referrerPolicy = 'no-referrer-when-downgrade';
+l.parentNode.insertBefore(s, l);
+})({})`,
+          }}
+          strategy="afterInteractive"
+        />
       </head>
       <body className="min-h-full flex flex-col bg-white font-sans text-zinc-900 antialiased">
         {children}
